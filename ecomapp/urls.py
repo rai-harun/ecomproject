11b6314cc063527e965390de4a5b4a04cf3e56ee
Harun <rai.harun64@gmail.com>
2020-11-17 22:09:28 +0545

    First Commit! Set up the project and set up settings with few urls, views, and templates

---
+++ ecomapp/urls.py
@@ -0,0 +1,10 @@
+from django.urls import path
+from .views import *
+
+app_name = "ecomapp"
+urlpatterns = [
+    path('', HomeView.as_view(), name="home"),
+    path('about/', AboutView.as_view(), name="about"),
+    path('contact-us/', ContactView.as_view(), name="contact")
+
+]
